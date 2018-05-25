f2a4cfebb93ee76682433d56460406a42ed9552d
Michael Pugh <pughy12.12@gmail.com>
2018-05-25 19:32:09 +0100

    Fixed auth header not being sent with API call

--- src/main/java/com/pug/ppm/service/ProductServiceImpl.java
+++ src/main/java/com/pug/ppm/service/ProductServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -33,7 +34,8 @@ public class ProductServiceImpl implements ProductService {
     public List<Product> getProducts() {
         final HttpEntity<String> requestEntity = new HttpEntity<>(buildAuthorizationHeader(apiUsername, apiPassword));
 
-        final ResponseEntity<List> products = restTemplate.getForEntity(PRODUCT_API_PRODUCTS_ENDPOINT, List.class);
+        final ResponseEntity<List> products = restTemplate.exchange(PRODUCT_API_PRODUCTS_ENDPOINT,
+                HttpMethod.GET, requestEntity, List.class);
 
         return products.getBody();
     }
